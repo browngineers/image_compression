8ecac565ce4eb160b343008806017133f080be6d
Rohan Chaudhry <rohan.chaudhry@utexas.edu>
2018-10-14 15:30:27 -0500

    RELEASE 2

--- image_sizer.py
+++ image_sizer.py
@@ -1,5 +1,6 @@
 import PIL,  resizeimage, glob, os
 from PIL import Image as IMG
+from PIL import ImageFilter
 from resizeimage import resizeimage
 pics = sorted(glob.glob('DSC_*.JPG'))  # input picture files are .JPG
 
@@ -15,6 +16,8 @@ def createFolder(directory):  # create folder to hold resized images within curr
 def resize_all():  # resize JPG images in the same folder as image_sizer.py
     for pic in pics:
         print('working on ' + pic)
+        bw(pic) # convert ot BW
+        sharp(pic) # add gaussian blur
         with open(pic, 'r+b') as f:
             with IMG.open(f) as picture:
                 new_name = 'resized_' + str(pic) + ".jpg" # saving as .jpg
@@ -24,15 +27,41 @@ def resize_all():  # resize JPG images in the same folder as image_sizer.py
                 cover = resizeimage.resize_contain(picture, [640,640])  # only resizing, no cropping
                 cover = cover.convert("RGB")
                 cover.save(new_name, picture.format, quality = 100, dpi = dpi_val)
+
                 picture.close()
 
 
 def move_files():  # move resized files to their own folder within main folder
     resized = sorted(glob.glob('resized_*.JPG'))
+    bw = sorted(glob.glob('BW_*.JPG'))
+    sharp = sorted(glob.glob('Sharp_*.JPG'))
     for pic in resized:
         src = pic
         dst = './ig_size/'+pic
         os.rename(src, dst)
+    for pic in bw:
+        src = pic
+        dst = './ig_size/'+pic
+        os.rename(src, dst)
+    for pic in sharp:
+        src = pic
+        dst = './ig_size/'+pic
+        os.rename(src, dst)
+
+
+def bw(pic):  # black and white
+    bw = IMG.open(pic)
+    bw = bw.convert('1') # convert to BW
+    bw.save('BW_' + str(pic) + '.jpg') # save as jpg
+    return
+
+
+def sharp(pic):  # black and white
+    sharp = IMG.open(pic)
+    #blur = blur.filter(ImageFilter.GaussianBlur(radius=50)) # add blur, radius = 50
+    sharp = sharp.filter(ImageFilter.UnsharpMask(radius=8, percent=150, threshold=3))  # add blur, radius = 50
+    sharp.save('Sharp_' + str(pic) + '.jpg') # save as jpg
+    return
 
 
 def main():
