0b2be0f049ca49d05ce2b01afafce85166d675e9
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-10-18 11:35:40 -0500

    ADD: adding server functionality

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const app = express()
 
+
+// HBS setup
+const hbs = require('express-handlebars')({
+  extname: '.hbs'
+});
+
+app.engine('hbs', hbs);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(fileUpload());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+    res.render('iter_1')
+})
+
+app.listen(3000, function(err) {
+    if(err) {
+        console.log('There was an error');
+    } else {
+        console.log('We are live on 3000 :)');
+    }
+
+})
